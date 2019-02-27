de002f074531445bec7a5b40951ea05e82d095f3
bkwillia052 <bkwillia052@gmail.com>
2019-02-27 12:57:29 -0600

    binary search tree complete

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,18 +1,21 @@
 class LRUCache:
-  def __init__(self, limit=10):
-    pass
+    def __init__(self, limit=10):
+        self.limit = limit
+        self.cache = []
 
-  """
+    """
   Retrieves the value associated with the given key. Also
   needs to move the key-value pair to the top of the order
   such that the pair is considered most-recently used.
   Returns the value associated with the key or None if the
   key-value pair doesn't exist in the cache. 
   """
-  def get(self, key):
-    pass
 
-  """
+    def get(self, key):
+
+        pass
+
+    """
   Adds the given key-value pair to the cache. The newly-
   added pair should be considered the most-recently used
   entry in the cache. If the cache is already at max capacity
@@ -22,5 +25,20 @@ class LRUCache:
   want to overwrite the old value associated with the key with
   the newly-specified value. 
   """
-  def set(self, key, value):
-    pass
+
+    def set(self, key, value):
+
+        for index, item in enumerate(self.cache):
+            if item[key] == value:
+                self.cache.pop(index)
+
+        pass
+
+    def see_cache(self):
+        print(self.cache)
+        print(len(self.cache))
+
+
+c = LRUCache()
+
+c.see_cache()
